7ba11319d339f0b3f8043693f8a8afdb3aa485c4
Ilknur Cakir <ilknur.chakir@gmail.com>
2023-08-25 12:49:00 -0700

    updated app run for cloud deployment

--- app.py
+++ app.py
@@ -78,4 +78,5 @@ app.include_router(api_router)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="localhost", port=8001)
+    # uvicorn.run(app, host="localhost", port=8001)
+    uvicorn.run("app", host="0.0.0.0", port=8000)
